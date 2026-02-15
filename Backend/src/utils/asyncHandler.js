bd7514fb778b74820c2590a68d7653950a05c75b
sanket111219 <dassanket11@gmail.com>
2026-02-16 00:19:03 +0530

    AsyncHandle and other utils

---
+++ Backend/src/utils/asyncHandler.js
@@ -0,0 +1,34 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise
+        .resolve(
+            requestHandler(req, res, next)
+        )
+        .catch(
+            (error) => {
+                next(error)
+            }
+        )
+    }
+    
+}
+
+
+export {asyncHandler}
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {}
+// const asyncHandler = (func) => {()=> {}}
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+
+//     }catch(errro){
+//         res.status(errro.code || 500).json({
+//             success: false,
+//             message: errro.message    
+//         })
+//     }
+// }
